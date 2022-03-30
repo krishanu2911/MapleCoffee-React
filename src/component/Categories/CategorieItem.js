9123543a77cbd5c400302097885c4b65ecf0dc23
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-03-30 11:49:58 +0530

    Fix-FewFeatures
    Added functionality
    -Clicking on category item cart redirecting to product listing page with filtered product
    -if the item is already present in cart and again add the item to cart from Wishlist increases it qty

--- src/component/Categories/CategorieItem.js
+++ src/component/Categories/CategorieItem.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import "../../App.css"
+import { useNavigate } from 'react-router-dom';
+import { useProduct } from '../../context/product-context';
 function CategorieItem( {itemDetail}) {
+    const categoryNavigate = useNavigate();
+    const {filterDispatch} = useProduct();
     return (
-        <div className="maple-flex-xy-center category-item txt-center">
-            <h1 className='txt-xl'>{itemDetail.categoryName}</h1>
+        <div className="maple-flex-xy-center category-item txt-center" onClick={() => {
+            filterDispatch({type:"CATEGORY", payload: itemDetail.categoryName})
+            categoryNavigate("./productlisting")
+        }}>
+            <h1 className='txt-xl'>{itemDetail.categoryTitle}</h1>
             <p className='text-centre'>{itemDetail.description}</p>
         </div>
     )
